e197d44838986a142c178205fea5360fe3344009
Nazif Malhi <nazifmalhi@gmail.com>
2023-02-02 16:43:44 +0500

    rebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Password } from "./layouts";
+import {
+  Loading,
+  Mockup,
+  Authentication,
+  VerifyAccount,
+  EmailSent,
+} from "./pages";
+import Admin from "./pages/Admin";
+import ProtectedRoute from "./ProtectedGaurd/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="farmstead" element={<Mockup />} />
+        <Route path="farmstead/authentication/*" element={<Authentication />} />
+        <Route path="farmstead/verify-email" element={<VerifyAccount />} />
+        <Route
+          path="farmstead/request/reset-password"
+          element={<EmailSent />}
+        />
+        <Route
+          path="farmstead/change/reset-password/*"
+          element={<Password />}
+        />
+        <Route
+          path="farmstead/admin/*"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Suspense>
   );
 }
 
